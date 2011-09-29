be74cbb00617ac3a8108d6f1c3e2276af1002672
Nat Pryce <github@natpryce.com>
2011-09-29 15:51:45 +0100

    added some unit-tests for there_is_a_line

--- src/tictactoe_test.py
+++ src/tictactoe_test.py
@@ -14,6 +14,18 @@ def there_is_a_line(moves):
         [(Top, Left), (Bottom, Left), (Top, Middle), (Bottom, Middle), (Top, Right)],
         [(Top, Left), (Bottom, Left), (Top, Middle), (Bottom, Middle), (Middle, Right), (Bottom, Right)]]
 
+
+def test_there_is_a_line_by_first_player_at_the_top():
+    assert there_is_a_line([(Top, Left), (Bottom, Left), (Top, Middle), (Bottom, Middle), (Top, Right)])
+
+def test_there_is_not_a_line_by_first_player_at_the_top():
+    assert not there_is_a_line([(Top, Left), (Bottom, Left), (Top, Middle), (Bottom, Middle), (Bottom, Right)])
+
+def test_there_is_a_line_by_second_player_at_the_bottom():
+    assert there_is_a_line(
+        [(Top, Left), (Bottom, Left), (Top, Middle), (Bottom, Middle), (Middle, Right), (Bottom, Right)])
+
+
 def who_wins(moves):
     if there_is_a_line(moves):
         if len(moves) == 5:
