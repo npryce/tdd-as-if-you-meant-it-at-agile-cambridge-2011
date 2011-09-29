457ec152cb067fc8867ff3f675566bccc1fee884
Nat Pryce <github@natpryce.com>
2011-09-29 14:24:42 +0100

    renamed tests to reflect what they are testing

--- src/tictactoe_test.py
+++ src/tictactoe_test.py
@@ -7,10 +7,10 @@ def who_wins(moves):
     return Nobody
 
 
-def test_when_nobody_wins():
+def test_nobody_has_won_when_nobody_has_moved():
     winner = who_wins([])
     assert winner == Nobody
 
-def test_when_player_x_plays_in_the_middle():
+def test_nobody_has_won_when_player_x_plays_in_the_middle():
     winner = who_wins([(X, Middle, Middle)])
     assert winner == Nobody
