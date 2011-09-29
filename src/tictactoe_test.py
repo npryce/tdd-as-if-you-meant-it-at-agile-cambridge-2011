d0f4a5c2ae6efd827ccf15be539dc895bd75d767
Nat Pryce <github@natpryce.com>
2011-09-29 14:31:12 +0100

    testing X wins, only for one case

--- src/tictactoe_test.py
+++ src/tictactoe_test.py
@@ -1,10 +1,16 @@
 
-Nobody = ""
+Nobody = "Nobody"
 X = "X"
+O = "O"
+
+Top = 0
+Left = 0
 Middle = 1
+Bottom = 2
+Right = 2
 
 def who_wins(moves):
-    return Nobody
+    return X if len(moves) == 5 else Nobody
 
 
 def test_nobody_has_won_when_nobody_has_moved():
@@ -14,3 +20,13 @@ def test_nobody_has_won_when_nobody_has_moved():
 def test_nobody_has_won_when_player_x_plays_in_the_middle():
     winner = who_wins([(X, Middle, Middle)])
     assert winner == Nobody
+
+def test_x_wins_after_minimal_number_of_moves():
+    winner = who_wins([
+            (X, Top, Left),
+            (O, Bottom, Left),
+            (X, Top, Middle),
+            (O, Bottom, Middle),
+            (X, Top, Right)])
+    
+    assert winner == X
