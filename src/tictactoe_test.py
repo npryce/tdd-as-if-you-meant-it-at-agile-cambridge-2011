56cd829f05a41334526fa12935f7f6e8a8c992fc
Nat Pryce <github@natpryce.com>
2011-09-29 14:13:25 +0100

    moved who_wins function to module level

--- src/tictactoe_test.py
+++ src/tictactoe_test.py
@@ -1,11 +1,11 @@
 
 Nobody = ""
 
+def who_wins():
+    return Nobody
+
 
 def test_when_nobody_wins():
-    def who_wins():
-        return Nobody
-    
     winner = who_wins()
     assert winner == Nobody
 
